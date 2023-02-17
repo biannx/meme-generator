e6bdb5840a5ac0e41442691f8c14365720fdc7d6
Bianca Aquino <lyahbianca.aquino04@gmail.com>
2023-02-17 19:27:53 +0800

    Get memes from API, used async, finished the proj.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/header.jsx'
 import Meme from './components/meme.jsx'
 
 function App() {
-
   return (
     <div className="App">
       <Header />
--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -1,81 +1,88 @@
-import React, {useState} from "react";
-import memesData from "../data.jsx"
+import React, { useState, useEffect } from "react";
 
 export default function Meme() {
 
-    const [meme, setMeme] = useState({
-        topText: "",
-        bottomText: "",
-        randomImage:"http://i.imgflip.com/1bij.jpg"
-    })
+	const [meme, setMeme] = useState({
+		topText: "",
+		bottomText: "",
+		randomImage: "http://i.imgflip.com/1bij.jpg"
+	})
 
-    console.log(Event)
-    console.log(meme)
-    const [allMemeImages, setAllMemeImages] = useState(memesData)
+	const [allMemes, setAllMemes] = useState([])
 
-    function getMemeImage(){
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length);
-        const url = memesArray[randomNumber].url
-        setMeme(prevMemes => ({
-            ...prevMemes,
-            randomImage: url
-        }))
-        console.log("changed!")
-    }
+	useEffect(() => {
+		async function getMemes(){
+			const response = await fetch("https://api.imgflip.com/get_memes")
+			const data = await response.json()
+			setAllMemes(data.data.memes)
+		}
+		getMemes()
+	}, [])
 
-    function handleChange(Event){
-        const {name, value} = Event.target
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            [name]:value
-        }))
-    }
 
-    return (
-        <main className="container my-5">
-            <div className="row">
-                {/* top text */}
-                <div className="col-lg-6 col-sm-12 form-floating mb-3">
-                    <input
-                        type="text"
-                        className="form-control px-3"
-                        id="floatingTop"
-                        placeholder="test"
-                        name="topText"
-                        onChange={handleChange}
-                        value={meme.topText}
-                    />
-                    <label htmlFor="floatingTop">Top text</label>
-                </div>
+	function getMemeImage() {
+		const randomNumber = Math.floor(Math.random() * allMemes.length);
+		const url = allMemes[randomNumber].url
+		setMeme(prevMemes => ({
+			...prevMemes,
+			randomImage: url
+		}))
+		console.log("changed!")
+	}
 
-                {/* bottom text */}
-                <div className="col-lg-6 col-sm-12 form-floating">
-                    <input
-                        type="text"
-                        className="form-control px-3"
-                        id="floatingBottom"
-                        placeholder="test"
-                        name="bottomText"
-                        onChange={handleChange}
-                        value={meme.bottomText}
-                    />
-                    <label htmlFor="floatingBottom">Bottom text</label>
-                </div>
-            </div>
-            <button 
-                type="button" 
-                className="w-100 py-2 my-3 text-light"
-                onClick={getMemeImage}
-            >
-                Get a new meme image 🖼
-            </button>
-            <div className="meme">
-                <img src={meme.randomImage} className="meme-image" alt="Meme Image" />
-                <h2 className="meme-text top">{meme.topText}</h2>
-                <h2 className="meme-text bottom">{meme.bottomText}</h2>
-            </div>
-            <br />
-        </main>
-    )
+	// controlled inputs for top and bottom text for every character stroke/value
+	function handleChange(Event) {
+		const { name, value } = Event.target
+		setMeme(prevMeme => ({
+			...prevMeme,
+			[name]: value
+		}))
+	}
+
+	return (
+		<main className="container my-5">
+			<div className="row">
+				{/* top text */}
+				<div className="col-lg-6 col-sm-12 form-floating mb-3">
+					<input
+						type="text"
+						className="form-control px-3"
+						id="floatingTop"
+						placeholder="test"
+						name="topText"
+						onChange={handleChange}
+						value={meme.topText}
+					/>
+					<label htmlFor="floatingTop">Top text</label>
+				</div>
+
+				{/* bottom text */}
+				<div className="col-lg-6 col-sm-12 form-floating">
+					<input
+						type="text"
+						className="form-control px-3"
+						id="floatingBottom"
+						placeholder="test"
+						name="bottomText"
+						onChange={handleChange}
+						value={meme.bottomText}
+					/>
+					<label htmlFor="floatingBottom">Bottom text</label>
+				</div>
+			</div>
+			<button
+				type="button"
+				className="w-100 py-2 my-3 text-light"
+				onClick={getMemeImage}
+			>
+				Get a new meme image 🖼
+			</button>
+			<div className="meme">
+				<img src={meme.randomImage} className="meme-image" alt="Meme Image" />
+				<h2 className="meme-text top">{meme.topText}</h2>
+				<h2 className="meme-text bottom">{meme.bottomText}</h2>
+			</div>
+			<br />
+		</main>
+	)
 }
